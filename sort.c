2b743e693e61d06cb45df567e771895e0274783a
jamiechoi1995 <no.23waterfish@163.com>
2018-04-11 18:03:43 +0800

    Improve sort.c by using fopen() and fread().

--- sort.c
+++ sort.c
@@ -8,27 +8,25 @@
 #define BLOCK 1024*1024
 double *data;
 int double_cmp(const void *a,const void *b){
-    return *(double *)a>*(double *)b?1:-1;
+    return *(double *)a > *(double *)b ? 1 : -1;
 }
 int main(int argc,char* argv[]){
     int loop;
-    int i,j;
-    if(argc!=3){
+    if(argc != 3){
         printf("Usage:%s loop_times data_file\n",argv[0]);
     }
-    loop=atoi(argv[1]);
-    int handle;
-    handle=open(argv[2],O_RDONLY);
-    if(handle==-1){
+    loop = atoi(argv[1]);
+    FILE *fp;
+    if((fp = fopen(argv[2],"r")) == 0){
         printf("Cannot Open file %s\n",argv[2]);
         return -1;
     }
     data=(double *)malloc(sizeof(double)*BLOCK*loop);
-    read(handle,data,BLOCK*loop*sizeof(double));
+    fread(data, sizeof(double) ,BLOCK*loop, fp);
 
-    qsort(data,loop*BLOCK,sizeof(double),double_cmp);
+    qsort(data, BLOCK*loop, sizeof(double), double_cmp);
 
-    printf("data[0]=%f last data=%f\n",data[0],data[loop*BLOCK-1]);
-    close(handle);
+    printf("data[0]=%f last data=%f\n", data[0], data[BLOCK*loop-1]);
+    fclose(fp);
 }
 
